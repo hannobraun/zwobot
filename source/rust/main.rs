2fc1ab92d8ab4b6215d1877dc8f58452e07cd24a
Hanno Braun <mail@hannobraun.de>
2014-02-17 10:40:39 +0100

    Implemented command-line argument parsing.

---
+++ source/rust/main.rs
@@ -0,0 +1,22 @@
+use std::os;
+
+
+fn main() {
+	let args = os::args();
+
+	let command = args[1].to_owned();
+	let files   = args.slice_from(2);
+
+	print_files(files);
+	print_command(command);
+}
+
+fn print_command(command: &str) {
+	print!("-> {}\n", command);
+}
+
+fn print_files(files: &[~str]) {
+	for file in files.iter() {
+		print!("{}\n", *file);
+	}
+}
