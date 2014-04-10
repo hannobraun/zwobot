b081413acec2fbf23f79e35c7444ff1fa2868078
Hanno Braun <mail@hannobraun.de>
2014-04-10 13:45:39 +0000

    Remove debug output

--- source/rust/zwobot/main.rs
+++ source/rust/zwobot/main.rs
@@ -14,9 +14,6 @@ fn main() {
 	let command = args[1].to_owned();
 	let files   = args.slice_from(2);
 
-	print_files(files);
-	print_command(command);
-
 	let inotify = match INotify::init() {
 		Ok(inotify) => inotify,
 		Err(error)  => fail!(error)
@@ -48,15 +45,5 @@ fn main() {
 	match inotify.close() {
 		Ok(_)      => (),
 		Err(error) => fail!(error)
-	}
-}
-
-fn print_command(command: &str) {
-	print!("-> {}\n", command);
-}
-
-fn print_files(files: &[~str]) {
-	for file in files.iter() {
-		print!("{}\n", *file);
 	}
 }
