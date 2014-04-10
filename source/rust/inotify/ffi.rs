8221d63276f581cea5f67682e21d3035cd423130
Hanno Braun <mail@hannobraun.de>
2014-04-10 11:45:50 +0000

    Refactor

--- source/rust/inotify/ffi.rs
+++ source/rust/inotify/ffi.rs
@@ -7,16 +7,6 @@ use libc::{
 	uint32_t };
 
 
-extern {
-	pub fn inotify_init() -> c_int;
-	pub fn inotify_init1(flags: c_int) -> c_int;
-	pub fn inotify_add_watch(fd: c_int, pathname: *c_char, mask: uint32_t) -> c_int;
-	pub fn inotify_rm_watch(fd: c_int, wd: c_int) -> c_int;
-	pub fn read(fd: c_int, buf: *c_void, count: size_t) -> ssize_t;
-	pub fn close(fd: c_int) -> c_int;
-}
-
-
 // Flags for inotify_init1()
 pub static IN_CLOEXEC : c_int = 0o2000000;
 pub static IN_NONBLOCK: c_int = 0o4000;
@@ -69,4 +59,14 @@ pub struct inotify_event {
 	pub cookie: uint32_t,
 	pub len   : uint32_t,
 	pub name  : *c_char
+}
+
+
+extern {
+	pub fn inotify_init() -> c_int;
+	pub fn inotify_init1(flags: c_int) -> c_int;
+	pub fn inotify_add_watch(fd: c_int, pathname: *c_char, mask: uint32_t) -> c_int;
+	pub fn inotify_rm_watch(fd: c_int, wd: c_int) -> c_int;
+	pub fn read(fd: c_int, buf: *c_void, count: size_t) -> ssize_t;
+	pub fn close(fd: c_int) -> c_int;
 }
