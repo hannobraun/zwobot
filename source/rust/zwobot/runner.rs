ec9751f8bd7ff4c751a83e225bee462810d92fa0
Hanno Braun <mail@hannobraun.de>
2014-05-22 09:30:57 +0000

    De-duplicate events
    Some text editors produce multiple file system events when they save a
    file. Zwobot handles this by delaying command execution by 50ms and
    canceling the execution if another one follows within that time.

--- source/rust/zwobot/runner.rs
+++ source/rust/zwobot/runner.rs
@@ -1,4 +1,9 @@
+use std::comm::{
+	Disconnected,
+	Empty
+};
 use std::io::Command;
+use std::io::timer;
 
 use inotify::ffi::inotify_event;
 
@@ -8,27 +13,61 @@ pub fn new(command_str: ~str) -> Sender<inotify_event> {
 		command_str.words().map(|x| x.to_owned()).collect();
 
 	let executable = command_words.get(0).clone();
-	let args       = command_words.tail();
+	let args       = command_words.tail().to_owned();
 
+	let (sender, receiver) = channel();
+
+	spawn(proc() {
+		let mut waiting_command = None;
+
+		loop {
+			let _ = receiver.recv();
+
+			match waiting_command {
+				Some(sender) => drop(sender),
+				None         => ()
+			}
+
+			waiting_command = Some(run(executable.clone(), args));
+		}
+	});
+
+	sender
+}
+
+fn run(executable: ~str, args: &[~str]) -> Sender<()> {
 	let mut command = Command::new(executable);
 	command.args(args);
 
 	let (sender, receiver) = channel();
 
 	spawn(proc() {
-		loop {
-			let _ = receiver.recv();
+		// Don't execute the command right away. Another event that triggers the
+		// same command might follow almost immediately. In that case, this run
+		// is going to be cancelled.
+		timer::sleep(50);
 
-			let mut process = match command.spawn() {
-				Ok(process) => process,
-				Err(error)  => fail!("{}", error)
-			};
+		match receiver.try_recv() {
+			Ok(_) => (),
 
-			let _ = process.wait();
-
-			print!("{}", process.stdout.take().expect("no stdout").read_to_str().unwrap());
-			print!("{}", process.stderr.take().expect("no stderr").read_to_str().unwrap());
+			Err(error) => match error {
+				Empty        => (),
+				Disconnected => {
+					// We're cancelled! Return!
+					return;
+				}
+			}
 		}
+
+		let mut process = match command.spawn() {
+			Ok(process) => process,
+			Err(error)  => fail!("{}", error)
+		};
+
+		let _ = process.wait();
+
+		print!("{}", process.stdout.take().expect("no stdout").read_to_str().unwrap());
+		print!("{}", process.stderr.take().expect("no stderr").read_to_str().unwrap());
 	});
 
 	sender
